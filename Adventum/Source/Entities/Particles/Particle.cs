a5b1e90642245990fd4871191ba8231b94f31906
Andrew Hunter <sockmonkey905@gmail.com>
2020-01-13 23:51:12 -0500

    Began on particles.

---
+++ Adventum/Source/Entities/Particles/Particle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using MonoGame.Extended;
+
+namespace Adventum.Entities.Particles
+{
+	public class Particle : Entity
+	{
+		public Particle(Vector2 position, Color color, Angle direction, float speed, float lifeSpan) : base(position)
+		{
+
+		}
+	}
+}
